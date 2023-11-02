1f3a1f47d5c6014ed9ab6a3f91e652e9c3f44cdc
jaerlcruz <jaeraelcruz@gmail.com>
2023-11-02 09:36:32 -0700

    Allowed resizing of canvas

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function initCanvas() {
+function initCanvas(size) {
     const canvas = document.querySelector('#canvas');
-    for (let i=0; i < 16; i++) {
+    for (let i=0; i < size; i++) {
         const col = document.createElement('div');
-        for (let j=0; j < 16; j++) {
+        col.className = "col"
+        for (let j=0; j < size; j++) {
         const div = document.createElement('div');
-        div.setAttribute('style', 'background: #fff; width: 50px; height: 50px;');
+        div.className = "pixel"
         div.onmouseover = function() {
             this.style.backgroundColor = 'gray';
         }
@@ -14,4 +15,17 @@ function initCanvas() {
     }
 }
 
-initCanvas()
+function changeCanvas() {
+    const canvas = document.querySelector('#canvas');
+    canvas.innerHTML = "";
+    const size = prompt("New canvas size?");
+    if (size > 100) {
+        alert("Try a smaller size please!")
+        initCanvas(100);
+    }
+    else {
+        initCanvas(size);
+    }
+}
+
+initCanvas(40)
